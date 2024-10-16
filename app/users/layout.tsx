2c69b97d6461a43cfe49b380596d70f6dd74a875
danielschneider22 <danielschneider22@gmail.com>
2024-10-16 13:31:23 -0400

    suspense and line endings

---
+++ app/users/layout.tsx
@@ -0,0 +1,9 @@
+import { Suspense } from 'react';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>;
+}
