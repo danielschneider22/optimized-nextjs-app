81e77f363f409950e37536477c3f1aabbd09cc3d
danielschneider22 <danielschneider22@gmail.com>
2024-10-16 21:18:54 -0400

    failing tests

--- app/users/userData.ts
+++ app/users/userData.ts
@@ -4,7 +4,7 @@ export const userData: User[] = new Array(100).fill(0).map((_, i) => {
   const id = i + 1;
   return {
     id: String(id),
-    email: `user${id}@gmail.com`,
+    email: `userasdfsadf${id}@gmail.com`,
     username: `user${id}`,
     createdAt: 'Wed Oct 16 2024',
   };
